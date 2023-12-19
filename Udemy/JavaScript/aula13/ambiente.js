feac2a859675e579ce550a09401a9a9b3156ee2d
RafaSilva025 <rafasilvasimoes25@gmail.com>
2023-12-19 08:40:36 -0300

    [ADD]- Curso de JS da Udemy.

---
+++ Udemy/JavaScript/aula13/ambiente.js
@@ -0,0 +1,14 @@
+// Estrutura de repetição 
+// while / do / for 
+var c = 1
+while (c <= 10) {
+     console.log(c)
+    c++
+}
+
+// Do while
+var c = 1
+do {
+    console.log(c)
+    c++
+}   while (c <= 5)
