9d49adadf5f10dcd8d7f4208ad387e62e9e2154e
RafaSilva025 <rafasilvasimoes25@gmail.com>
2023-12-19 08:35:36 -0300

    [ADD]- Começo de Curso de JS na Udemy

---
+++ Udemy/JavaScript/Operadores_Logicos.js
@@ -0,0 +1,17 @@
+/*
+Operadores de comparação 
+>= maior que ou igual a
+<= menor que ou igual a 
+== igualdade (valor) *********
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) *********
+!== diferente estrito (valor e tipo)
+/*
+
+/*
+Operadores lógicos
+&& --> AND --> E
+|| --> OR --> OU
+! -> NOT --> Não
+*/
+ 
